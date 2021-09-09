c03e72061eb0c27dccb8e422c27734b24ac52e44
brookenicole51 <bhavens@csumb.edu>
2021-09-08 17:23:10 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,25 @@
+//How to make Guacamole
+	//Cut 3 avocados in half
+		//Remove the seeds from the avocados
+		//Scoop out avocados into large mixing bowl
+		//Mash avocados until you get desired guac consistency
+	//Cut onion in half
+		//Dice half of onion
+		//Add to medium mixing bowl
+	//Cut jalapeño in half
+		//Remove seeds from jalapeño
+		//Dice jalapeno
+		//Add to the same medium mixing bowl
+	//Cut of ends of two tomatoes
+		//Dice up the two tomatoes
+		//Add to the same medium mixing bowl
+	//Remove stems from cilantro
+		//Chop 1/4 cup cilantro
+		//Add to the same medium mixing bowl
+	//Cut lime in half
+		//Squeeze lime over mixing bowl ingredients
+		//Sprinkle salt into mixing bowl
+	//Mix ingredients in the medium mixing bowl
+//Add medium mixing bowl ingredients into large mixing bowl with avocado
+	//Stir all ingredients until evenly mixed
+	//Add more ingredients for desired taste
