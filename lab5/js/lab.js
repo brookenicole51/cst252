7a80ad42253b2eb9c67a525773ffd95dfd6b9528
brookenicole51 <70400933+brookenicole51@users.noreply.github.com>
2021-09-13 16:39:09 -0700

    first steps lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,9 @@
+//Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004//
+
+//Declare a boolean variable ownIt for whether you own it or not.//
+
+//Once you declare them, reference them and see them printed out in the console.//
+
+//Create a new variable age that subtracts the year from the current year.//
+
+//Use console.log() to print the value of age, example, console.log("Age: " + age)//
