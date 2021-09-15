0efd443ac8a22933e8f616ac04540097701fb6ea
brookenicole51 <70400933+brookenicole51@users.noreply.github.com>
2021-09-14 22:54:50 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,9 +1,27 @@
+/*
+* Author: Brooke Havens
+* Created: Spetember 13, 2021
+* License: Public Domain
+*/
+
 //Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004//
+var make = "Kia";
+var model = "Sportage";
+var color = "Black";
+var year = 2012;
+var currentYear = 2021;
 
 //Declare a boolean variable ownIt for whether you own it or not.//
-
-//Once you declare them, reference them and see them printed out in the console.//
+var ownIt = true;
 
 //Create a new variable age that subtracts the year from the current year.//
+var age = currentYear - year
 
-//Use console.log() to print the value of age, example, console.log("Age: " + age)//
+//Use document.writeln() to print the value of age, example, console.log("Age: " + age)//
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Current year: " + currentYear + "<br>");
+document.writeln("Own it: " + ownIt + "<br>");
+document.writeln("Age: " + age + "<br>");
