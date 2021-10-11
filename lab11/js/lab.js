b8bf2f418c5595dd49c50e5b1e672dcd432ae7a8
brookenicole51 <70400933+brookenicole51@users.noreply.github.com>
2021-10-10 22:35:02 -0700

    lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,14 @@
+/*
+ * Author: Brooke Havens
+ * Created: 21 September
+ * License: Public Domain
+ */
+
+//Create a <button> element
+$(document).ready(function () {
+    $("#btn").click(function () {
+        alert("So you just do what you're told?");
+    });
+});
+
+$("#btn").css("background", "lightgreen")
