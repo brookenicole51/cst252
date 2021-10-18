c7b353dc80f587dd987ee1055112381fad2f3098
brookenicole51 <70400933+brookenicole51@users.noreply.github.com>
2021-10-18 10:42:55 -0700

    lab 13

---
+++ lab13/js/lab.js
@@ -0,0 +1,31 @@
+/**
+ * Author: Brooke Havens
+ * Created:10/18/2021
+ * License: Public Domain
+ **/
+
+ // Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+ function sortingHat(str) {
+   len = str.length;
+   mod = len % 4;
+   if (mod == 0) {
+     return "Gryffindor"
+   }
+ else if (mod == 1) {
+   return "Ravenclaw"
+ }
+ else if (mod == 2) {
+   return "Slytherin"
+ }
+ else if (mod == 3) {
+   return "Hufflepuff"
+ }
+}
+
+ var myButton = document.getElementById("button");
+ myButton.addEventListener("click", function () {
+   var name = document.getElementById("input").value;
+   var house = sortingHat(name);
+   newText = "<p>The sorting hat has sorted you into " + house + "</p>";
+   document.getElementById("output").innerHTML = newText;
+ })
