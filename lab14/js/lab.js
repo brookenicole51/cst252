52012ac7f1994642766e6947eeafb2e7d682079c
brookenicole51 <70400933+brookenicole51@users.noreply.github.com>
2021-10-27 09:36:16 -0700

    lab 15

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -5,24 +5,44 @@
  **/
 
 //Loop through numbers 1 to 200, listing them as you go
-for (var num=1; num <= 200; num++) {
-  var outputStr = ""
+for (i=1; i <= 200; i++) {
+  var outputEl = document.getElementById("output") ;
+  var writeNumber = document.createElement("p");
+
+  if (i%105 == 0) {
+    writeNumber.innerHTML = "FizzBuzzBoom";
+    outputEl.appendChild(writeNumber)
+    console.log("FizzBuzzBoom");
+  } else if (i%35 == 0) {
+    writeNumber.innerHTML = "BuzzBoom";
+    outputEl.appendChild(writeNumber)
+    console.log("BuzzBoom");
+  } else if (i%21 == 0) {
+    writeNumber.innerHTML="FizzBoom";
+    outputEl.appendChild(writeNumber)
+    console.log ("FizzBoom");
+  } else if (i%15 == 0) {
+    writeNumber.innerHTML="FizzBuzz"
+    outputEl.appendChild(writeNumber)
+    console.log ("FizzBuzz");
 //If the number is a multiple of 3, if should print "Fizz!"
-  if (num % 3 == 0) {
-    outputStr += "Fizz"
-  }
+  } else if (i%3 == 0) {
+    writeNumber.innerHTML = "FizzBuzz";
+    outputEl.appendChild(writeNumber)
+    console.log("Fizz");
 //If the number is a multiple of 5, it should print "Buzz!"
-  if (num % 5 == 0) {
-    outputStr += "Buzz"
-  }
+  } else if (i%5 == 0) {
+    writeNumber.innerHTML = "Buzz";
+    outputEl.appendChild(writeNumber)
+    console.log("Buzz");
 //If the number is a multiple of 7, it should print "Boom!"
-  if (num % 7 == 0) {
-    outputStr += "Boom"
-  }
-  if (outputStr == "") {
-    console.log(num);
-  }
-  else {
-    console.log(outputStr + '!');
+  } else if (i%7 == 0) {
+    writeNumber.innerHTML = "Boom";
+    outputEl.appendChild(writeNumber)
+    console.log("Boom");
+  } else {
+    writeNumber.innerHTML = i;
+    outputEl.appendChild(writeNumber)
+    console.log(i);
   }
 }
